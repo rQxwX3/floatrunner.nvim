de55c643473e8bed609e3b9a340651516446356f
rQxwX3 <github@scrt.simplelogin.com>
2025-07-21 21:22:21 +0300

    Add tests for get_run_cmd() and get_build_cmd()

--- tests/test_floatrunner.lua
+++ tests/test_floatrunner.lua
@@ -4,7 +4,17 @@ local eq = MiniTest.expect.equality
 local noeq = MiniTest.expect.no_equality
 
 local T = new_set()
-T["setup()"] = new_set()
+
+T["setup()"] = new_set({
+	hooks = {
+		pre_case = function()
+			pcall(vim.api.nvim_del_keymap, "n", "floaterm_on")
+			pcall(vim.api.nvim_del_user_command, "FloatRunner")
+			package.loaded["floatrunner.init"] = nil
+			package.loaded["floatrunner.config"] = nil
+		end
+	}
+})
 
 T["setup()"]["sets user options"] = function()
 	require("floatrunner.init").setup({
@@ -27,8 +37,10 @@ T["setup()"]["registers user commands"] = function()
 	require("floatrunner.init").setup({})
 
 	local cmds = vim.api.nvim_get_commands({})
+	local user_cmd = cmds["FloatRunner"]
 
-	noeq(cmds["FloatRunner"], nil)
+	noeq(user_cmd, nil)
+	eq(user_cmd.nargs, "1")
 end
 
 T["setup()"]["creates user keymaps"] = function()
@@ -45,6 +57,60 @@ T["setup()"]["creates user keymaps"] = function()
 	end
 
 	eq(found, true)
+end
+
+T["utils"] = new_set({
+	hooks = {
+		pre_case = function()
+			package.loaded["floatrunner.internal.fileutils"] = nil
+			package.loaded["floatrunner.internal.buildutils"] = nil
+		end
+	}
+})
+
+
+T["utils"]["get_run_cmd()"] = function()
+	vim.cmd("enew")
+
+	vim.api.nvim_buf_set_name(0, "main.c")
+
+	local langs = {
+		{ exts = { "c" }, command = "command", argv = {} }
+	}
+
+	local res = require("floatrunner.internal.fileutils").get_run_cmd(langs)
+
+	eq(res, "command")
+end
+
+
+T["utils"]["get_build_cmd()"] = function()
+	local tmpfile = "/tmp/floatrunner-test/build-test.c"
+	local tmpdir = "/tmp/floatrunner-test"
+
+	vim.cmd("enew")
+
+	vim.api.nvim_buf_set_name(0, tmpfile)
+	vim.fn.mkdir("/tmp/floatrunner-test", "p")
+	vim.loop.chdir(tmpdir)
+	vim.fn.writefile({ "# Dummy Makefile" }, "/tmp/floatrunner-test/Makefile")
+
+	local buildutils = require("floatrunner.internal.buildutils")
+
+	buildutils.builds_cache = {}
+
+	local builds = { ["Makefile"] = "make command" }
+
+	vim.cmd("edit " .. tmpfile)
+
+	local result = buildutils.get_build_cmd(builds)
+
+	noeq(result, nil)
+	eq(result.path, vim.fn.resolve(tmpdir))
+	eq(result.command, "make command")
+
+	local cached = buildutils.get_cached_build(tmpdir)
+	eq(cached, result)
 end
 
 return T
